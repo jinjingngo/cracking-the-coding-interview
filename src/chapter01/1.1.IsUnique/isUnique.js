ef30d0e09ab8c43a17f686cfa426d9b6b0fff196
wujinjing <wujinjing@kedacom.com>
2021-01-26 22:02:29 +0800

    begin to conqure

---
+++ src/chapter01/1.1.IsUnique/isUnique.js
@@ -0,0 +1,12 @@
+// O(N^2) approach, no additional data structures used
+// for each character, check remaining characters for duplications. 
+export const isUniqueCharacters = (str) => {
+   for (var i = 0; i < str.length; i++) {
+       for (var j = i + 1; j < str.length; j++) {
+           if (str[i] === str[j]) {
+               return false;
+           }
+       }
+   }
+   return true;
+};
