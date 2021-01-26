4e246317d07888ed4b66c98a22881c50962c41d0
charleserious <wujinjing25@gmail.com>
2021-01-27 00:15:42 +0800

    conquer is permutation

---
+++ test/chapter01/1.2.IsPermutation/isPermutation.spec.js
@@ -0,0 +1,19 @@
+import chai from 'chai';
+import * as funcs from '../.././../src/chapter01/1.2.IsPermutation/isPermutation.js';
+const { expect } = chai;
+
+for (let key in funcs) {
+  const func = funcs[key];
+  describe(`chapter01-1.2 ${key}`, function () {
+    [
+      [ 'abcdefghi', 'ihgfedcba' ],
+      [ '1a1', '1a1' ],
+      [ '1234567812345678', '8877665544332211' ],
+      [ 'icarraci', 'carcarii' ]
+    ].forEach(args => {
+      it(`returns true for string that are permutation '${args[0]}' & '${args[1]}'`, function () {
+        expect(func(args[0].split(''), args[1].split(''))).to.be.true;
+      });
+    });
+  });
+}
