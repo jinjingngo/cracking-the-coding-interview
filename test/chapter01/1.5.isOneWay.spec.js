baec16a05fed7766123a74a7f0a26c1a5d694b44
charleserious <wujinjing25@gmail.com>
2021-02-01 23:07:38 +0800

    conquer is oneway edit

---
+++ test/chapter01/1.5.isOneWay.spec.js
@@ -0,0 +1,37 @@
+import chai from 'chai';
+import * as funcs from '../../src/chapter01/1.5.isOneWay.js'
+const { expect } = chai;
+
+for (let key in funcs) {
+  const func = funcs[key];
+
+  describe(`chapter01-1.5 ${key}`, function () {
+    it('returns true for the same string', function () {
+      let str = 'abcdef';
+      expect(func(str, str)).to.be.true;
+      str = 'a1b2c3d4';
+      expect(func(str, str)).to.be.true;
+    });
+
+    [
+      ['pale', 'ple'],
+      ['pales', 'pale'],
+      ['pale', 'bale'],
+      ['pale', 'pxle'],
+      ['pale', 'pate'],
+      ['pale', 'pald'],
+      ['answers', 'answer'],
+      ['technology', 'etechnology']
+    ].forEach(args => {
+      it(`returns true for strings that are one edit: '${args[0]}' & '${args[1]}'`, function () {
+        expect(func(args[0], args[1])).to.be.true;
+      });
+    });
+
+    [].forEach(args => {
+      it(`returns false for string that are more than one edit: '${args[0]}' & '${args[1]}'`, function () {
+        expect(func(args[0], args[1])).to.be.false;
+      });
+    });
+  });
+}
