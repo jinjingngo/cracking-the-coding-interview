58c71d60885fcd8153bc177422d1d14fbe15ad7d
wujinjing <wujinjing@kedacom.com>
2021-03-27 20:35:09 +0800

    some experiment on partition

--- test/chapter02/2.4.partition.spec.js
+++ test/chapter02/2.4.partition.spec.js
@@ -31,22 +31,22 @@ for (let key in funcs) {
       {
         list: [4, 9, 15, 30, 5, 8, 3, 2, 2, 3, 7, 1, 15, 5, 6, 7],
         value: 6,
-        expect: [4, 5, 3, 2, 2, 3, 1, 5, 9, 15, 30, 8, 7, 15, 6, 7]
+        expected: [4, 5, 3, 2, 2, 3, 1, 5, 9, 15, 30, 8, 7, 15, 6, 7]
       },
       {
         list: [4, 9, 15, 30, 5, 8, 3, 2, 2, 3, 7, 1, 15, 5, 6, 7],
         value: 17,
-        expect: [4, 9, 15, 5, 8, 3, 2, 2, 3, 7, 1, 15, 5, 6, 7, 30]
+        expected: [4, 9, 15, 5, 8, 3, 2, 2, 3, 7, 1, 15, 5, 6, 7, 30]
       },
       {
         list: [4, 9, 15, 30, 5, 8, 20, 25, 20, 21, 21, 7, 1, 15, 5, 6, 7],
         value: 14,
-        expect: [4, 9, 5, 8, 7, 1, 5, 6, 7, 15, 30, 20, 25, 20, 21, 21, 15]
+        expected: [4, 9, 5, 8, 7, 1, 5, 6, 7, 15, 30, 20, 25, 20, 21, 21, 15]
       }
     ].forEach(context => {
       it(`partitioning list ${context.list} around ${context.value}`, function () {
         const list = helpers.arrayToLinkedList(context.list);
-        expect(helpers.linkedListToArray(func(list, context.vaue))).to.eql(context.expected);
+        expect(helpers.linkedListToArray(func(list, context.value))).to.eql(context.expected);
       });
     });
   });
